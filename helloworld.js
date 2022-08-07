9a7acc725109fb053bb7dc11e9b7f479675c9696
SwayamGos <59536611+SwayamGos@users.noreply.github.com>
2022-08-07 17:33:06 +0530

    Add files via upload

---
+++ helloworld.js
@@ -0,0 +1,4 @@
+alert("Hello World")
+let js="amazing";
+if(js="amazing")
+alert("js is fun");
---
+++ intro.js
@@ -0,0 +1,5 @@
+//javascript is a high level, object oriented, multi paradigm programming language.
+//(Basically JS is goated)
+//html->skeleton
+//css->design
+//js->interactiveness
